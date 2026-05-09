844e469c4618114ac6cc53a88ee5c060913a8691
Son Luong Ngoc <sluongng@gmail.com>
2026-05-09 20:39:57 +0200

    [bes bep 1/3]: add BEP format
    Some BES consumers expect Bazel Build Event Protocol events instead of
    Buck2 native events. Add the converter and format selection because the
    BES sink needs to publish the event shape requested by configuration.

--- app/buck2_client_ctx/src/subscribers/recorder.rs
+++ app/buck2_client_ctx/src/subscribers/recorder.rs
@@ -417,6 +417,8 @@ impl InvocationRecorder {
                 bes_backend: None,
                 #[cfg(not(fbcode_build))]
                 bes_headers: Vec::new(),
+                #[cfg(not(fbcode_build))]
+                event_format: Default::default(),
             },
             critical_path_backend: None,
             bxl_ensure_artifacts_duration: None,
@@ -631,6 +633,8 @@ impl InvocationRecorder {
                 bes_backend: None,
                 #[cfg(not(fbcode_build))]
                 bes_headers: Vec::new(),
+                #[cfg(not(fbcode_build))]
+                event_format: Default::default(),
             },
         );
         self.health_check_tags_receiver = health_check_tags_receiver;
