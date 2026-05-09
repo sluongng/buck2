6e22e5180342b8e4948e83613f87bfe5109a377c
Son Luong Ngoc <sluongng@gmail.com>
2026-05-09 20:39:56 +0200

    [bes core 3/5]: show invocation URLs
    Users need a direct way to open the remote BES invocation after a
    build starts publishing events. Surface the URL because the uploaded
    invocation should be reachable from normal Buck2 output and log replay.

--- app/buck2_client_ctx/src/streaming.rs
+++ app/buck2_client_ctx/src/streaming.rs
@@ -37,6 +37,7 @@ use crate::daemon::client::connect::connect_buckd;
 use crate::events_ctx::EventsCtx;
 use crate::exit_result::ExitResult;
 use crate::path_arg::PathArg;
+use crate::remote_sink_config;
 use crate::signal_handler::with_simple_sigint_handler;
 use crate::subscribers::build_graph_stats::BuildGraphStats;
 use crate::subscribers::build_id_writer::BuildIdWriter;
@@ -62,6 +63,7 @@ fn update_events_ctx<T: StreamingCommand>(
     let expect_spans = cmd.should_expect_spans();
 
     let paths = ctx.paths().ok();
+    let bes_results_url = remote_sink_config::bes_results_url(paths);
 
     // Need this to get information from one subscriber (event_log)
     // and log it in another (invocation_recorder)
@@ -104,6 +106,7 @@ fn update_events_ctx<T: StreamingCommand>(
         T::COMMAND_NAME,
         console_opts.superconsole_config(),
         health_check_display_reports_receiver,
+        bes_results_url,
     );
     subscribers.push(console_subscriber);
     events_ctx.used_superconsole = used_superconsole;
